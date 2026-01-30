8b0ff33a8fb041fdb34229e8243fbd9399def780
benjuwan_5931 <benjuwan.kokusaig@gmail.com>
2026-01-30 12:03:59 +0900

    refactor: 同一ファイル名の画像データはリネーム及びリサイズ処理後に別途フォルダに移動させる処理実装.

--- utils/adjustedfile_copy_to_numbering_dir.py
+++ utils/adjustedfile_copy_to_numbering_dir.py
@@ -3,7 +3,8 @@ import os
 import shutil
 
 
-def _move_to_numbering_dir(duplicated_imgfiles: list[str]) -> None:
+# 重複ファイル（同一ファイル名）を別フォルダへ移動
+def _copy_to_numbering_dir(duplicated_imgfiles: list[str]) -> None:
     try:
         NUMBERING_DIR = "../src/_dist/_numbering"
 
@@ -15,30 +16,28 @@ def _move_to_numbering_dir(duplicated_imgfiles: list[str]) -> None:
             origin_filename = os.path.basename(imgfile)
             numbering_filename = f"{i + 1}__{origin_filename}"
 
-            # 連番付与したファイルパスを生成
+            # 連番付与したファイルパスを生成し、当該フォルダへとコピー
             dest_path = os.path.join(NUMBERING_DIR, numbering_filename)
-
             shutil.copy2(imgfile, dest_path)
 
-            # 連番生成された重複ファイルだけ、連番生成前（オリジン）のファイル名で`../src/_dist`に残しておきたい
+            # 連番付与前の画像が不要な場合は、上記`copy2`のコードを消して以下一行を有効化する
+            # shutil.move(imgfile, dest_path)
 
-            # imgfile が src/_dist に存在する場合は削除
+            # `imgfile`が`src/_dist`に存在する場合、該当画像ファイル名を表示
             if NUMBERING_DIR.split("/_numbering")[0] in imgfile:
-                os.remove(imgfile)
+                print(f"- {imgfile} には同一ファイル名が存在しています。")
+
+                # 連番付与前の画像が不要な場合はこの一行、または上記`shutil.move`の部分を有効化する
+                # os.remove(imgfile)
 
     except Exception as e:
-        print(f"_move_to_numbering_dir エラー | {e}")
+        print(f"_copy_to_numbering_dir エラー | {e}")
         return
 
 
 # リネーム及びリサイズ後、ファイル名に "__" が含まれている場合
 # `../src/_dist/_numbering`フォルダへ移動
-def adjustedfile_move_to_numbering_dir(
-    origin_file: str | None = None, img_file: str = "../src/_dist"
-) -> None:
-    if origin_file is None:
-        return
-
+def adjustedfile_copy_to_numbering_dir(img_file: str = "../src/_dist") -> None:
     try:
         # 同一ファイル名の画像ファイルパス格納リスト
         duplicated_imgfiles = []
@@ -57,7 +56,7 @@ def adjustedfile_move_to_numbering_dir(
                 duplicated_imgfiles.append(img)
 
         if len(duplicated_imgfiles) > 0:
-            _move_to_numbering_dir(duplicated_imgfiles)
+            _copy_to_numbering_dir(duplicated_imgfiles)
 
     except Exception as e:
         print(f"リネーム・リサイズ後のファイル移動エラー | {e}")
@@ -65,4 +64,4 @@ def adjustedfile_move_to_numbering_dir(
 
 
 if __name__ == "__main__":
-    adjustedfile_move_to_numbering_dir()
+    adjustedfile_copy_to_numbering_dir()
--- utils/rename_img.py
+++ utils/rename_img.py
@@ -3,14 +3,14 @@ import os  # ファイル操作を行う
 
 
 # リネーム処理
-def rename_jpg(img_file: str | None = None) -> str | None:
-    if img_file is None:
+def rename_jpg(origin_filepath: str | None = None) -> str | None:
+    if origin_filepath is None:
         return None
 
     try:
         # 新しいファイル名を生成（単なる文字列の編集）
         # 文字列を正規化（NFKCで合成済み文字として扱う）
-        normalized_path = unicodedata.normalize("NFKC", img_file)
+        normalized_path = unicodedata.normalize("NFKC", origin_filepath)
         new_filepath = (
             normalized_path.replace("ページ", "page")
             if normalized_path.count("ページ") > 0
@@ -20,7 +20,7 @@ def rename_jpg(img_file: str | None = None) -> str | None:
         # ファイルの実際の名前を変更（パスとして扱うために必要な別処理）
         # 置換対象の文字列が画像ファイル名に存在しない場合、
         # 渡ってきた画像パスのまま返却（＝何も処理せずそのまま返却される）
-        os.rename(img_file, new_filepath)
+        os.rename(origin_filepath, new_filepath)
 
         return new_filepath
 
